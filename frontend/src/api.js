a7a894a3c181a0c340118268d97912eb629476a0
Zubii07 <zohaibrasheed983@gmail.com>
2025-04-19 16:02:32 +0500

    Add backend URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { ACCESS_TOKEN } from './constants';
 
 
-const apiURL="https://4800b1fc-ba30-4d51-9d1e-d4ac122c6f03-dev.e1-us-east-azure.choreoapis.dev/notes-app/backend/v1"
+const apiURL="/choreo-apis/notes-app/backend/v1"
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiURL,
 })
