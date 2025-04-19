5b53d2173ec731f465b657c9ebb2695330755bab
Zubii07 <zohaibrasheed983@gmail.com>
2025-04-19 15:58:48 +0500

    Add Backend URL in frontend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 import { ACCESS_TOKEN } from './constants';
 
+
+const apiURL="https://4800b1fc-ba30-4d51-9d1e-d4ac122c6f03-dev.e1-us-east-azure.choreoapis.dev/notes-app/backend/v1"
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiURL,
 })
 
 api.interceptors.request.use((config) => {
