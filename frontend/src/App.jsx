06444cf855632be28394a9f7f6c83961837a2981
Zubii07 <zohaibrasheed983@gmail.com>
2025-04-19 16:25:31 +0500

    Update form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,18 +13,19 @@ function Logout() {
 }
 
 
-function RegisterAndLogout() {
+/*function RegisterAndLogout() {
   localStorage.clear();
   return <Register />;
-}
+}*/
+
 function App() {
   return (
     <BrowserRouter> 
       <Routes>  
         <Route path="/" element={<ProtectedRoute><Home /></ProtectedRoute>} />
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/logout" element={<Logout />} />
-        <Route path="/register" element={<RegisterAndLogout />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
 
--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import api from "../api";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants";
 import "../styles/Form.css"
 import LoadingIndicator from "./LoadingIndicator";
@@ -54,6 +54,11 @@ function Form({ route, method }) {
             <button className="form-button" type="submit">
                 {name}
             </button>
+            {method === "login" && (
+            <p style={{ marginTop: "1rem", textAlign: "center" }}>
+                Don't have an account? <Link to="/register">Register</Link>
+            </p>
+        )}
         </form>
     );
 }
